eb2065892df68656000d513f5f38ebcfa5f2b67a
ttomov88 <ttomov88@gmail.com>
2026-09-24 21:17:34 +0300

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,10 @@
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCEEFr5OacmCb-qMhE2m70xTfDzA6TMU9c",
+  authDomain: "fast-track-app-tt.firebaseapp.com",
+  projectId: "fast-track-app-tt",
+  storageBucket: "fast-track-app-tt.firebasestorage.app",
+  messagingSenderId: "368091780461",
+  appId: "1:368091780461:web:2eb68c7d222a9b67fdf664Ready when you are. What are we diving into today?
